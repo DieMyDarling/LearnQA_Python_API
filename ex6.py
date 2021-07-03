3e01157a0dc48c7f438e12a959caaf1250d72810
Aleksey <sjim2ez4u@gmail.com>
2021-07-03 15:37:09 +0400

    Ex 6. Long redirect

---
+++ ex6.py
@@ -0,0 +1,10 @@
+import requests
+
+
+r = requests.get('https://playground.learnqa.ru/api/long_redirect')
+
+redirects = len(r.history)  # Считаем редиректы
+print(redirects)
+
+final_url = r.history[-1].url
+print(final_url)
