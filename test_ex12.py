4871c10f23e2821d18ca39c75cd0c5cc5633e615
DieMyDarling <ppc69L0l!>
2021-07-03 22:58:14 +0400

    Ex 12. Headers value

---
+++ test_ex12.py
@@ -0,0 +1,10 @@
+import requests
+
+
+url = 'https://playground.learnqa.ru/api/homework_header'
+
+
+def test_ex12():
+    print(requests.head(url).headers)
+    assert requests.head(url).headers['x-secret-homework-header'] == 'Some secret value'
+
