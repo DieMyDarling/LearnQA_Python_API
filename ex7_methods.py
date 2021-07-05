e6cc34de77e9ab91762f275b6921afc250714c33
DieMyDarling <ppc69L0l!>
2021-07-05 19:57:44 +0400

    Ex7. Methods

--- ex7_methods.py
+++ ex7_methods.py
@@ -6,8 +6,8 @@ method = ('GET', 'POST', 'PUT', 'DELETE')
 
 head = requests.head(url, data={'method':'HEAD'}).text
 empty_get = requests.get(url, params={}).text
-print(head)
-print(empty_get)
+print('head' + head)
+print('empty_get ' + empty_get)
 
 for i in range(0, 4):
     get = requests.get(url, params={"method": method[i]}).text
