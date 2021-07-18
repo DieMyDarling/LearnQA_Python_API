da80557cfc9b325994213a0ee4c8fcfca9dedb4b
DieMyDarling <ppc69L0l!>
2021-07-19 00:45:47 +0400

    ex.15. Almost done

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -21,3 +21,58 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_incorrect_email(self):
+        email = 'vinkotovexample.com'
+        data = self.prepare_registration_data(email)
+        response = MyRequests.post("/user", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"Invalid email format", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_short_name(self):
+        data = self.prepare_registration_data()
+        data['username'] = 'l'
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The value of 'username' field is too short", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_long_name(self):
+        data = self.prepare_registration_data()
+        data[
+            'username'] = 'ThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongNameThisIsVeryFuckingLongName'
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The value of 'username' field is too long", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_empty_fields(self):
+        data = self.prepare_registration_data()
+        data[f'username'] = None
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: username", f"Unexpected response content {response.content}"
+
+        data = self.prepare_registration_data()
+        data['password'] = None
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: password", f"Unexpected response content {response.content}"
+
+        data = self.prepare_registration_data()
+        data['firstName'] = None
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: firstName", f"Unexpected response content {response.content}"
+
+        data = self.prepare_registration_data()
+        data['lastName'] = None
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: lastName", f"Unexpected response content {response.content}"
