6352019bde512fa028d9000f1559dd13110f71e4
DieMyDarling <ppc69L0l!>
2021-07-19 11:29:46 +0400

    ex15. Added use of parameterization

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,22 @@
+import pytest
+
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
 
 
 class TestUserRegister(BaseCase):
+    data_with_one_empty_param = [
+        ({'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'vinkotov@example.com'},
+         'password'),
+        ({'password': '123', 'firstName': 'learnqa', 'lastName': 'learnqa', 'email': 'vinkotov@example.com'},
+         'username'),
+        ({'password': '123', 'username': 'learnqa', 'lastName': 'learnqa', 'email': 'vinkotov@example.com'},
+         'firstName'),
+        ({'password': '123', 'username': 'learnqa', 'firstName': 'learnqa', 'email': 'vinkotov@example.com'},
+         'lastName'),
+        ({'password': '123', 'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa'}, 'email')
+    ]
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
@@ -48,12 +61,10 @@ class TestUserRegister(BaseCase):
         assert response.content.decode(
             "utf-8") == f"The value of 'username' field is too long", f"Unexpected response content {response.content}"
 
-    def test_create_user_with_empty_fields(self):
-        params = {'username', 'password', 'firstName', 'lastName'}
-        for param in params:
-            data = self.prepare_registration_data()
-            data[f'{param}'] = None
-            response = MyRequests.post("/user", data=data)
-            Assertions.assert_code_status(response, 400)
-            assert response.content.decode(
-                "utf-8") == f"The following required params are missed: {param}", f"Unexpected response content {response.content}"
+    @pytest.mark.parametrize("data", data_with_one_empty_param)
+    def test_create_user_with_empty_fields(self, data):
+        data, empty_param = data
+        response = MyRequests.post("/user", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The following required params are missed: {empty_param}", f"Unexpected response content {response.content}"
